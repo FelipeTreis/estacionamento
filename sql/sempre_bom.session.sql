b0c90c851159beeb002aed813ba031fd9a68989f
Felipe Treis <felipetreis1@gmail.com>
2022-05-16 12:00:50 -0300

    Add id on table

--- sql/sempre_bom.session.sql
+++ sql/sempre_bom.session.sql
@@ -1,5 +1,5 @@
 CREATE TABLE estacionamento(
-    id INTEGER PRIMARY NOT NULL DEFAULT nextval('estacionamento_id_seq'::regclass)
+    id SERIAL PRIMARY KEY,
     veiculo VARCHAR(9) NOT NULL,
     tempo INT NOT NULL,
     valor NUMERIC CHECK(valor > 0)NOT NULL
